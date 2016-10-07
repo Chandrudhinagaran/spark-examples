d24591291d15422ae2796ef5afe1ba91ada7c2c7
arpitrathore <arpit.cloud@gmail.com>
2016-10-07 09:36:55 +0530

    Some Spark examples

--- src/main/scala/org/arpit/spark/examples/PopulatTwitterHashTags.scala
+++ src/main/scala/org/arpit/spark/examples/PopulatTwitterHashTags.scala
@@ -7,38 +7,24 @@ import org.apache.spark.streaming.twitter.TwitterUtils
 object PopulatTwitterHashTags {
 
   def main(args: Array[String]): Unit = {
-    setupTwitter()
+    Utility.setupTwitter()
     val ssc = new StreamingContext("local[*]", "PopulatTwitterHashTags", Seconds(1))
-    setupLogging()
+    Utility.setupLogging()
     
     val tweets = TwitterUtils.createStream(ssc, None)
     val sortedResults = tweets.map(status => status.getText())
                                  .flatMap(tweetText => tweetText.split(" "))
                                  .filter(word => word.startsWith("#"))
                                  .map(hashtag => (hashtag, 1))
-                                 .reduceByKeyAndWindow((x, y) => x + y, (x, y) => x - y, Seconds(300), Seconds(5))
+                                 .reduceByKeyAndWindow((x : Int, y:Int) => x + y, Seconds(300), Seconds(5))
                                  .transform(rdd => rdd.sortBy(x => x._2, false))
     sortedResults.print
     startSparkStreaming(ssc)
   }
   
-  
   def startSparkStreaming(ssc : StreamingContext) = {
     ssc.checkpoint("/home/arathore/checkpoint")
     ssc.start()
     ssc.awaitTermination()
-  }
-
-  def setupLogging() = {
-    import org.apache.log4j.{ Level, Logger }
-    val rootLogger = Logger.getRootLogger()
-    rootLogger.setLevel(Level.ERROR)
-  }
-
-  def setupTwitter() = {
-    System.setProperty("twitter4j.oauth.consumerKey", "WFGoXZ8YVVZy1owb638xqwgds")
-    System.setProperty("twitter4j.oauth.consumerSecret", "nbxIHKBEXeJGlyHVAk504IqU8BpCiowTyu8qaw3nHG5BFbITof")
-    System.setProperty("twitter4j.oauth.accessToken", "756431275720863745-kZcz6W4bTL3Vz97fZWHHK9jCFUOH45w")
-    System.setProperty("twitter4j.oauth.accessTokenSecret", "u1sAnf6XYSljgjbK2nHmB6uml5B8xJec2xcwgrb32L8xY")
   }
 }
--- src/main/scala/org/arpit/spark/examples/WordCount.scala
+++ src/main/scala/org/arpit/spark/examples/WordCount.scala
@@ -6,13 +6,19 @@ import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
 
 object WordCount {
-  
+
   def main(args: Array[String]): Unit = {
 
     val conf = new SparkConf().setMaster("local[*]").setAppName("Spark word count")
     val sc = new SparkContext(conf)
+    Utility.setupLogging()
 
     val textFile = sc.textFile("/home/arathore/scala_wiki.txt")
+
+//      .filter(_.startsWith("i"))
+//      .map(_.toUpperCase())
+//      .sortBy(x => x._2, false)
+    
 
     val counts = textFile.flatMap(line => line.split(" "))
       .map(word => (word, 1))
